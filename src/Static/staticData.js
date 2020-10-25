bf23932d1a703281bfe7d6ffb2cc0c7ec11f9967
danomov <dx555r@gmail.com>
2020-10-25 14:54:29 +0400

    data correction

--- src/Static/staticData.js
+++ src/Static/staticData.js
@@ -18,20 +18,11 @@ const data = {
               Have around 2 year experience with JavaScript, ReactJS, NodeJS, HTML/CSS, Git.
               Passionate about writing articles about new technologies and programming.
           `,
-    portrait:
-      "https://drive.google.com/uc?export=view&id=1fm5J7iWEUkpDYjP_C-NsWvDgAMeVMpgA",
+    portrait: "https://drive.google.com/uc?export=view&id=1fm5J7iWEUkpDYjP_C-NsWvDgAMeVMpgA",
   },
   skills: {
     title: "Skills",
-    skillList: [
-      "JavaScript",
-      "ReactJS",
-      "React-Redux",
-      "NodeJS",
-      "HTML/CSS",
-      "SCSS",
-      "Git",
-    ],
+    skillList: ["JavaScript", "ReactJS", "React-Redux", "NodeJS", "HTML/CSS", "SCSS", "Git"],
   },
   experience: {
     title: "Experience",
@@ -94,7 +85,7 @@ const data = {
       {
         name: "Cheng-Tsui App",
         description: `Cheng & Tsui is the leading publisher of Asian language learning textbooks & multimedia educational materials.
-        Cheng-Tsui (CT) App is an "online school" for teaching your students or learning asian language with Cheng & Tsui 
+        Cheng-Tsui (CT) App is an "online school" for teaching your students or learning th Asian language with Cheng & Tsui 
         books with your classmates.`,
         links: [
           {
@@ -104,7 +95,7 @@ const data = {
       },
       {
         name: "4Tat Website",
-        description: `4Tat is an simple, but modern online store where you can find food for your lovely pets.`,
+        description: `4Tat is a simple, but modern online store where you can find food for your lovely pets.`,
         links: [
           {
             link: "https://4tat.am/",
@@ -114,7 +105,7 @@ const data = {
       {
         name: "Nextstack Website",
         description: `A small dynamic website for IT company, 
-        where you can investigate company's portfolio and career opportunities.`,
+        where you can investigate the company's portfolio and career opportunities.`,
         links: [
           {
             link: "https://nextstack.am/",
@@ -138,19 +129,18 @@ const data = {
       {
         name: "DS and AI",
         description: `DS and AI is a growing source where you can find different 
-        Data Structures and Algorithm Implementations in JavaScript programming language.`,
+        Data Structures and Algorithm Implementations in the JavaScript programming language.`,
         links: [
           {
-            link:
-              "https://github.com/danomov/DataStructuresAndAlgorithmImplementations",
+            link: "https://github.com/danomov/DataStructuresAndAlgorithmImplementations",
           },
         ],
       },
       {
         name: "ATS",
         description: `Automation Traffic System (ATS) is an automatic traffic system visual representation application written in React. 
-        With this app you can simply test automatic traffic system behaviour in different situations by simply inputting cars' 
-        quantity in all sides of virtual crossroad.`,
+        With this , you can simply test automatic traffic system behavior in different situations by simply inputting cars' 
+        quantity on all sides of the virtual crossroad.`,
         links: [
           {
             link: "https://github.com/danomov/AutomaticTrafficSystem",
@@ -161,7 +151,7 @@ const data = {
         name: "npm CRA danomov_template",
         description: `Create React App is a tool (built by developers at Facebook) that gives you a
         massive head start when building React apps. It saves you from time-consuming setup and configuration.
-        With "danomov_template" template you can boost the starting process of your project further by structuring 
+        With the "danomov_template" you can boost the starting process of your project further by structuring 
         your project's preset view.`,
         links: [
           {
@@ -175,7 +165,7 @@ const data = {
     title: "Contact",
     info: {
       title: "Get In Touch",
-      description: `My inbox is always open. Whether you have a question or just want to say hi, 
+      description: `Any questions, suggestions, or just a little discussion? I am always open. 
       I'll try my best to get back to you!`,
       mail: "danielmovsesyan.dev@gmail.com",
       phone: "+37499207778",
