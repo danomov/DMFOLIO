1bdd40bf267dd79f4157c533ed262cafeb776654
danomov <dx555r@gmail.com>
2020-11-25 22:13:03 +0400

    change/F-5

--- src/Static/staticData.js
+++ src/Static/staticData.js
@@ -83,17 +83,6 @@ const data = {
     title: "Projects",
     projectList: [
       {
-        name: "Cheng-Tsui App",
-        description: `Cheng & Tsui is the leading publisher of Asian language learning textbooks & multimedia educational materials.
-        Cheng-Tsui (CT) App is an "online school" for teaching your students or learning th Asian language with Cheng & Tsui 
-        books with your classmates.`,
-        links: [
-          {
-            link: "https://app.chengtsui.co/",
-          },
-        ],
-      },
-      {
         name: "4Tat Website",
         description: `4Tat is a simple, but modern online store where you can find food for your lovely pets.`,
         links: [
@@ -187,6 +176,7 @@ const data = {
         "https://medium.com/@danielmovsesyan",
         "https://www.facebook.com/danomov/",
       ],
+      resumeLink: "https://drive.google.com/file/d/1MukZvHDN9aEV6Lpl4Fl22RtHI0NKi8pH/view?usp=sharing",
     },
   },
 };
